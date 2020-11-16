22fd3928296dac2ed3d6bc056e89146f7b036680
overaction <52850392+overaction@users.noreply.github.com>
2020-11-16 14:03:18 +0900

    8. 14502_연구소 (성능개선)

--- 14502 연구소/main.cpp
+++ 14502 연구소/main.cpp
@@ -11,77 +11,32 @@ int dx[4] = {0,0,-1,1};
 deque < pair < int,int > > virusq;
 deque < pair < int,int > > shieldq;
 
-int calculate() {
-    int cnt = 0;
+void spreadVirus(int y, int x) {
+    for(int i=0; i<4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny > N || ny <= 0 ||nx > M || nx <= 0) continue;
+        if(virus[ny][nx] == 0) {
+            virus[ny][nx] = 2;
+            spreadVirus(ny,nx);
+        }
+    }
+}
+
+void calculate() {
+    int num = 0;
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
-            if(virus[i][j] == 0) cnt++;
+            if(virus[i][j] == 0) num++;
         }
     }
-    return cnt;
-}
-
-void tempArr(int arr[][9]) {
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=M; j++) {
-            arr[i][j] = virus[i][j];
-        }
-    }
-}
-
-void returnArr(int arr[][9]) {
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=M; j++) {
-            virus[i][j] = arr[i][j];
-        }
-    }
-}
-
-void bfs() {
-    deque < pair < int,int > > dq;
-    for(int i=0; i<virusq.size(); i++) {
-        int y = virusq[i].first;
-        int x = virusq[i].second;
-        dq.push_back(make_pair(y,x));
-    }
-    while(!dq.empty()) {
-        int y = dq.front().first;
-        int x = dq.front().second;
-        dq.pop_front();
-        for(int i=0; i<4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny > N || ny <= 0 || nx > M || nx <= 0) continue;
-            if(virus[ny][nx] == 0) {
-                dq.push_back(make_pair(ny,nx));
-                virus[ny][nx] = 2;
-            }
-        }
-    }
-}
-
-void dfs(int cnt) {
-    int temp[9][9];
-    tempArr(temp);
-    if(cnt == 3) {
-        bfs();
-        int num = calculate();
-        maxSafe = (maxSafe <= num) ? num : maxSafe;
-        return;
-    }
-    for(int i=0; i<shieldq.size(); i++) {
-        int y = shieldq[i].first;
-        int x = shieldq[i].second;
-        if(virus[y][x] == 0) {
-            virus[y][x] = 1;
-            dfs(cnt+1);
-            returnArr(temp);
-        }
-    }
+    maxSafe = (maxSafe <= num) ? num : maxSafe;
 }
 
 int main() {
+    int virus_origin[9][9];
     scanf("%d %d",&N,&M);
+    // 바이러스,안전지역 따로 저장
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
             scanf("%1d",&virus[i][j]);
@@ -93,6 +48,31 @@ int main() {
             }
         }
     }
-    dfs(0);
+    // 원본 저장
+    for(int i=1; i<=N; i++)
+        for(int j=1; j<=M; j++)
+            virus_origin[i][j] = virus[i][j];
+
+    // 임의의 3개 지역에 벽 세우기
+    int sqSize = shieldq.size();
+    for(int i=0; i<sqSize; i++)
+        for(int j=i+1; j<sqSize; j++)
+            for(int k=j+1; k<sqSize; k++) {
+                virus[shieldq[i].first][shieldq[i].second] = 1;
+                virus[shieldq[j].first][shieldq[j].second] = 1;
+                virus[shieldq[k].first][shieldq[k].second] = 1;
+
+                // 바이러스 퍼뜨리기
+                for(int v=0; v<virusq.size(); v++) {
+                    spreadVirus(virusq[v].first,virusq[v].second);
+                }
+                // 안전지역 계산
+                calculate();
+                // 원본 복구
+                for(int i=1; i<=N; i++)
+                    for(int j=1; j<=M; j++)
+                        virus[i][j] = virus_origin[i][j];
+            }
+
     printf("%d",maxSafe);
 }
