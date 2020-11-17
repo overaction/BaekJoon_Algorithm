872d96f94e3d2e3f623449dc9fbf8232a34a9121
overaction <52850392+overaction@users.noreply.github.com>
2020-11-17 23:21:57 +0900

    12. 7562_나이트의 이동(BFS)
    최단거리 문제는 DFS로 풀기 힘들다

--- 1987_알파벳/main.cpp
+++ 1987_알파벳/main.cpp
@@ -12,6 +12,7 @@ int dy[4] = {-1,1,0,0};
 int dx[4] = {0,0,-1,1};
 
 void calculate(int y, int x, int cnt) {
+    maximum = (maximum <= cnt) ? cnt : maximum;
     for(int i=0; i<4; i++) {
         int ny = y +dy[i];
         int nx = x +dx[i];
@@ -19,7 +20,6 @@ void calculate(int y, int x, int cnt) {
         int num = graph[ny][nx] - 65;
         if(dq[num].size() >= 1) continue;
         dq[num].push_back(1);
-        maximum = (maximum <= cnt) ? cnt : maximum;
         calculate(ny,nx,cnt+1);
         dq[num].pop_back();
     }
---
+++ 7562 나이트의 이동/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iostream>
+#include <stdio.h>
+#include <deque>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+int T,I,startx,starty,endx,endy,minimum = 999;
+int chess[301][301];
+int visited[301][301];
+int dy[8] = {-2,-1,-2,-1,2,1,2,1};
+int dx[8] = {-1,-2,1,2,-1,-2,1,2};
+
+void knight(int Y, int X) {
+    if(ny == endy && nx == endx) {
+        minimum = 0;
+        return;
+    }
+    deque < pair < int,int > > dq;
+    dq.push_back(make_pair(Y,X));
+
+    while(!dq.empty()) {
+        int y = dq.front().first;
+        int x = dq.front().second;
+        dq.pop_front();
+        for(int i=0; i<8; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny >= I || ny < 0 || nx >= I || nx < 0) continue;
+            if(ny == endy && nx == endx) {
+                minimum = (minimum >= chess[y][x]) ? chess[y][x] : minimum;
+                return;
+            }
+            if(chess[ny][nx] != 0) continue;
+            chess[ny][nx] = chess[y][x] + 1;
+            dq.push_back(make_pair(ny,nx));
+        }
+    }
+}
+
+
+int main() {
+    scanf("%d",&T);
+    for(int t=0; t<T; t++) {
+        scanf("%d",&I);
+        scanf("%d %d",&startx,&starty);
+        chess[starty][startx] = 1; // 현재 위치
+        scanf("%d %d",&endx,&endy);
+
+        knight(starty,startx);
+
+        printf("%d\n",minimum);
+        minimum = 9999; // 초기화
+        memset(chess,0,sizeof(chess));
+    }
+}
