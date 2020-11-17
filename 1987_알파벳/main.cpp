f43c2508d53ad2c330524a07d81b7176bc4a1d5e
overaction <52850392+overaction@users.noreply.github.com>
2020-11-17 19:31:10 +0900

    11. 1987_알파벳 (백트래킹)

---
+++ 1987_알파벳/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iostream>
+#include <stdio.h>
+#include <deque>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+int R,C,maximum = 0;
+int graph[21][21];
+deque < int > dq[26];
+int dy[4] = {-1,1,0,0};
+int dx[4] = {0,0,-1,1};
+
+void calculate(int y, int x, int cnt) {
+    for(int i=0; i<4; i++) {
+        int ny = y +dy[i];
+        int nx = x +dx[i];
+        if(ny >= R || ny < 0 || nx >= C || nx < 0 ) continue;
+        int num = graph[ny][nx] - 65;
+        if(dq[num].size() >= 1) continue;
+        dq[num].push_back(1);
+        maximum = (maximum <= cnt) ? cnt : maximum;
+        calculate(ny,nx,cnt+1);
+        dq[num].pop_back();
+    }
+}
+
+int main() {
+    char str[21];
+    scanf("%d %d",&R,&C);
+    for(int i=0; i<R; i++) {
+        scanf("%s",&str);
+        for(int j=0; j<C; j++) {
+            graph[i][j] = str[j];
+        }
+    }
+    int init = graph[0][0] - 65;
+    dq[init].push_back(1);
+    calculate(0,0,1);
+    printf("%d",maximum);
+}
