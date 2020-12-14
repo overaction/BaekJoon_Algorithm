316a60d686fdad6f322e17a656af9dd52bd64fe4
overaction <52850392+overaction@users.noreply.github.com>
2020-12-14 13:02:57 +0900

    26. 2636_치즈

--- 2636_치즈/main.cpp
+++ 2636_치즈/main.cpp
@@ -9,90 +9,99 @@ int visited[101][101];
 int dy[4] = {-1,1,0,0};
 int dx[4] = {0,0,-1,1};
 int result = 0;
-deque < pair < int,int > > cq;
-deque < pair < int,int > > melt;
-void bfs(int sy, int sx) {
-    visited[sy][sx] = 1;
+int answer = 0;
+deque < pair < int,int > > cheeseq;
+deque < pair < int,int > > meltq;
+void calculate() {
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=M; j++) {
+            if(cheese[i][j] == 1)
+                result++;
+        }
+    }
+}
+
+void setAir() {
+    memset(visited,0,sizeof(visited));
     deque < pair < int,int > > dq;
-    dq.push_back(make_pair(sy,sx));
+    dq.push_back(make_pair(1,1));
     while(!dq.empty()) {
         int y = dq.front().first;
         int x = dq.front().second;
         dq.pop_front();
-        int ismelt = 0;
         for(int i=0; i<4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if(cheese[ny][nx]==1 && visited[ny][nx]==0) {
-                dq.push_back(make_pair(ny,nx));
-                visited[ny][nx] = 1;
-            }
-
-            while(ny <= N && ny >= 1 && nx <= M && nx >= 1) {
-                if(ny == N || nx == M || ny == 1 || nx == 1) {
-                    ismelt = 1;
-                    break;
-                }
-                if(cheese[ny][nx]) break;
-                ny += dy[i];
-                nx += dx[i];
-            }
-            }
-
-        if(ismelt) {
-            melt.push_back(make_pair(y,x));
+            if(visited[ny][nx]) continue;
+            if(ny > N || ny <= 0 || nx > M || nx <= 0) continue;
+            if(cheese[ny][nx] == 1) continue;
+            cheese[ny][nx] = 3; // 공기
+            dq.push_back(make_pair(ny,nx));
+            visited[ny][nx] = 1;
         }
     }
 }
-int main()
-{
+
+void melting(int sy, int sx) {
+    deque < pair <int,int> > dq;
+    dq.push_back(make_pair(sy,sx));
+    visited[sy][sx] = 1;
+    while(!dq.empty()) {
+        int y = dq.front().first;
+        int x = dq.front().second;
+        dq.pop_front();
+        for(int i=0; i<4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny > N || ny <= 0 || nx > M || nx <= 0) continue;
+            if(cheese[ny][nx] == 3) {
+                cheese[y][x] = 2; // 다음에 녹을 치즈
+            }
+            if(visited[ny][nx] == 0 && cheese[ny][nx] == 1){
+                visited[ny][nx] = 1;
+                dq.push_back(make_pair(ny,nx));
+            }
+        }
+    }
+}
+
+void meltandfind() {
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=M; j++) {
+            if(cheese[i][j] == 2) {
+                cheese[i][j] = 3;
+            }
+            else if(cheese[i][j] == 1)
+                cheeseq.push_back(make_pair(i,j));
+        }
+    }
+}
+
+int main() {
     scanf("%d %d",&N,&M);
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
             scanf("%d",&cheese[i][j]);
-            if(cheese[i][j]) cq.push_back(make_pair(i,j));
+            if(cheese[i][j] == 1) cheeseq.push_back(make_pair(i,j));
         }
     }
-    int cqsize = cq.size();
     while(1) {
-        printf("\n");
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=M; j++) {
-                printf("%d ",cheese[i][j]);
-        }
-        printf("\n");
-    }
-        for(int i=0; i<cqsize; i++) {
-            int y = cq[i].first;
-            int x = cq[i].second;
-            if(visited[y][x] == 0) {
-                bfs(y,x);
-            }
-        }
-        result = cqsize;
-        memset(visited,0,sizeof(visited));
-        cq.clear();
+        int qsize = cheeseq.size();
+        if(qsize == 0) break;
         time++;
-        // 치즈 녹이기
-        int meltSize = melt.size();
-        printf("melt %d\n",meltSize);
-        for(int i=0; i<meltSize; i++) {
-            int y = melt[i].first;
-            int x = melt[i].second;
-            cheese[y][x] = 0;
+        // 바깥공기 검색
+        setAir();
+        result = 0;
+        calculate();
+        // 바깥공기에 의해 녹을 치즈들 검색
+        for(int i=0; i<qsize; i++) {
+            int y = cheeseq[i].first;
+            int x = cheeseq[i].second;
+            if(visited[y][x] == 0) melting(y,x);
         }
-        melt.clear();
-        // 남은 치즈 다시 넣기
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=M; j++) {
-                if(cheese[i][j]) cq.push_back(make_pair(i,j));
-            }
-        }
-        cqsize = cq.size();
-        if(cqsize == 0) {
-            break;
-        }
+        cheeseq.clear();
+        // 녹임
+        meltandfind();
     }
-    printf("%d\n",time);
-    printf("%d",result);
+    printf("%d\n%d",time,result);
 }
