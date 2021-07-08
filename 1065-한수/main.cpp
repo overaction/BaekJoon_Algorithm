c374f5f96ba1415d7268c772342d05875611ee01
mckim <kmc04@naver.com>
2021-07-08 10:21:45 +0900

    1065-한수

---
+++ 1065-한수/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int N;
+
+int solution(int N) {
+    if(N < 100) return N;
+    int cnt = 99;
+    for(int i=100; i<=N; i++) {
+        int a= i / 100;
+        int b = i / 10 % 10;
+        int c = i % 10;
+
+        if((c-b) == (b-a)) cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    scanf("%d",&N);
+    printf("%d",solution(N));
+}
