d20b99c48c3f6a0c970166ec081e462ff10210a2
mckim <kmc04@naver.com>
2021-08-09 14:30:18 +0900

    1516-게임 개발 (위상 정렬)

---
+++ 1516-게임 개발/1516-게임 개발/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int b[100];
+
+void solution() {
+    int a[100];
+    printf("%d %d\n",a[0],a[1]);
+}
+
+int main()
+{
+    solution();
+    printf("%d %d\n",b[0],b[1]);
+}
