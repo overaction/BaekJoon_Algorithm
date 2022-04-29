ae4835bec0bb3fa48183c8dafe716a71dd4901fb
mckim <kmc04@naver.com>
2022-04-29 22:27:18 +0900

    2504 - 괄호의 값 recap

--- 백준 - 구현/2504_괄호의 값/main.cpp
+++ 백준 - 구현/2504_괄호의 값/main.cpp
@@ -1,87 +1,52 @@
 #include <iostream>
 #include <stdio.h>
-#include <deque>
+#include <vector>
 #include <stack>
 #include <string>
 using namespace std;
-int N,result=0;
-bool possible = true;
-deque < int > dq;
 
-void solution() {
-    stack <int> s;
+vector < char > v;
+vector < char > calcV;
+int solution() {
+    int result = 0;
     int temp = 1;
-    int dqsize = dq.size();
-    for(int i=0; i<dqsize; i++) {
-        int next = dq[i];
-        // '('
-        if(next == 0) {
-            s.push(next);
+    int vSize = v.size();
+    for(int i=0; i<vSize; i++) {
+        char now = v[i];
+        if(now == '(') {
+            calcV.push_back('(');
             temp *= 2;
         }
-        // '['
-        else if(next == 2) {
-            s.push(next);
+        else if(now == '[') {
+            calcV.push_back('[');
             temp *= 3;
         }
-        // ')'
-        else if(next == 1) {
-            if(s.empty()) {
-                possible = false;
-                break;
+        else if(now == ')') {
+            if(calcV.empty() || calcV.back() != '(') return 0;
+            if(v[i-1] == '(') {
+                result += temp;
             }
-            else if(s.top() == 0) {
-                s.pop();
-                if(dq[i-1] == 0) result += temp;
-                temp = temp / 2;
-            }
-            else {
-                possible = false;
-                break;
-            }
+            calcV.pop_back();
+            temp /= 2;
         }
-        // ']'
-        else if(next == 3) {
-            if(s.empty()) {
-                possible = false;
-                break;
+        else if(now == ']') {
+            if(calcV.empty() || calcV.back() != '[') return 0;
+            if(v[i-1] == '[') {
+                result += temp;
             }
-            else if(s.top() == 2) {
-                s.pop();
-                if(dq[i-1] == 2) result += temp;
-                temp = temp / 3;
-            }
-            else {
-                possible = false;
-                break;
-            }
+            calcV.pop_back();
+            temp /= 3;
         }
     }
-    if(!s.empty()) possible = false;
+    if(!calcV.empty()) return 0; 
+    return result;
 }
 
-int main()
-{
+int main() {
     string s;
     cin >> s;
-    int sl = s.length();
-    for(int i=0; i<sl; i++) {
-        switch(s.at(i)) {
-        case '(':
-            dq.push_back(0);
-            break;
-        case ')':
-            dq.push_back(1);
-            break;
-        case '[':
-            dq.push_back(2);
-            break;
-        case ']':
-            dq.push_back(3);
-            break;
-        }
+    for(int i=0; i<s.length(); i++) {
+        v.push_back(s[i]);
     }
-    solution();
-    if(possible) printf("%d",result);
-    else printf("0");
+    printf("%d\n",solution());
 }
