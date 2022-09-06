d2954b0e196c2169ccad400d20ddb7c3acc5a603
overaction <kmc04@naver.com>
2022-09-06 23:44:02 +0900

    프로그래머스 - 2016년 c++ / 날짜계산

---
+++ 프로그래머스/프로그래머스 - 2016년/main.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string solution(int a, int b) {
+    string answer = "";
+    string day[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int sum = 0;
+    
+    // 월 더하기
+    for(int i=0; i<a; i++) {
+        sum += month[i];
+    }
+    sum += b-1;
+    answer = day[sum%7];
+    return answer;
+}
