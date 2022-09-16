23dd1c2d0df177790cc35a4dfbbeae2b26ac7a5e
overaction <kmc04@naver.com>
2022-09-17 02:55:54 +0900

    프로그래머스 - JadenCase 문자열 만들기 c++ / 문자열

---
+++ 프로그래머스/프로그래머스 - JadenCase 문자열 만들기/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    answer += toupper(s[0]);
+    for(int i=1; i<s.size(); i++) {
+        if(s[i-1] == ' ') answer += toupper(s[i]);
+        else answer += tolower(s[i]);
+    }
+    return answer;
+}
